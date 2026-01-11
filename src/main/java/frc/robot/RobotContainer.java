c4830fafcac60331ba43ade57cbab41c38c9a809
CodeCube314 <rs3codepi314@gmail.com>
2026-01-11 12:43:46 -0800

    2026 reimport

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,38 @@
+package frc.robot;
+
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+public class RobotContainer {
+    private final CommandXboxController driver = new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
+    public SwerveSubsystem drivebase = new SwerveSubsystem();
+
+    public RobotContainer() {
+        configureBindings();
+    drivebase.setDefaultCommand(driveFieldOrientedAngularVelocity);
+    }
+    SwerveInputStream driveAngularVelo = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                              ()-> driver.getLeftY()*-1,
+                                                              ()-> driver.getLeftX()*-1)
+                                                              .withControllerRotationAxis(driver::getRightX)
+                                                              .deadband(Constants.stickDeadband)
+                                                              .scaleTranslation(1.5)
+                                                              .allianceRelativeControl(true);
+                                                               
+    SwerveInputStream driveHeading = driveAngularVelo.copy().withControllerHeadingAxis(driver::getRightX,
+                                                                                       driver::getRightY)
+                                                                                       .headingWhile(true);
+
+
+    Command driveFieldOrientedDirectAngle = drivebase.driveFieldOriented(driveHeading);
+    Command driveFieldOrientedAngularVelocity = drivebase.driveFieldOriented(driveAngularVelo);
+    private void configureBindings() {
+        driver.start().onTrue(new InstantCommand(()->{drivebase.YawReset();},drivebase));
+    }
+    public Command getAutonomousCommand() {
+        return null; // fill in later
+    }
+}
